bfca2fab765071a7110ea7447e09c01a678cf2a7
Maximilian Weber <weber.maximilian@googlemail.com>
2016-05-31 17:42:57 +0200

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv/gcloud.storage "0.1.1-SNAPSHOT"
+(defproject sv/gcloud.storage "0.1.1"
   :description "A client for Google Cloud Storage."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
